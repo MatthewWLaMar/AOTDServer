4039145d7bd24280372fb563b16bdc6f9ea343cf
MatthewWLaMar <84155160+MatthewWLaMar@users.noreply.github.com>
2021-11-16 11:55:13 -0500

    trying to fix

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  host: 'localhost',
+  dialect: 'postgres'
+  
 })
 
 module.exports = db;
